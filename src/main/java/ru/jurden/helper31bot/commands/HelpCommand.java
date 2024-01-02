96ffcc796942b02c3ea03fa630f9c5fb95853416
jurden <dev@jurden.ru>
2024-01-03 01:56:53 +0300

    Added Help command

--- src/main/java/ru/jurden/helper31bot/commands/HelpCommand.java
+++ src/main/java/ru/jurden/helper31bot/commands/HelpCommand.java
@@ -1,9 +1,13 @@
 package ru.jurden.helper31bot.commands;
 
 import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
 
 @Slf4j
 public class HelpCommand extends Command {
@@ -17,7 +21,15 @@ public class HelpCommand extends Command {
         Chat chat = getUpdate().getMessage().getChat();
         SendMessage message = new SendMessage();
         message.setChatId(chat.getId());
-        message.setText("Hi!1123");
+        message.setParseMode(ParseMode.HTML);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("/help - this help\n");
+        builder.append("/password - generate password\n");
+        builder.append("/password help - generate password help\n");
+
+        message.setText(builder.toString());
+
         return message;
     }
 
