3fde4c26a621260eb8401826105cbe959a1b43d3
jurden <dev@jurden.ru>
2024-01-06 00:59:06 +0300

    Added Bot Menu

--- src/main/java/ru/jurden/helper31bot/telegram/Helper31Bot.java
+++ src/main/java/ru/jurden/helper31bot/telegram/Helper31Bot.java
@@ -4,17 +4,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import ru.jurden.helper31bot.commands.CommandFactory;
 import ru.jurden.helper31bot.config.BotConfig;
 import ru.jurden.helper31bot.repository.BotRepository;
 import ru.jurden.helper31bot.service.NoticeService;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public final class Helper31Bot extends TelegramLongPollingBot {
 
     final BotConfig botConfig;
@@ -32,6 +36,23 @@ public final class Helper31Bot extends TelegramLongPollingBot {
         return botConfig.getToken();
     }
 
+    public Helper31Bot(BotConfig botConfig, BotRepository botRepository, CommandFactory commandFactory, NoticeService noticeService) {
+        this.botConfig = botConfig;
+        this.botRepository = botRepository;
+        this.commandFactory = commandFactory;
+        this.noticeService = noticeService;
+        List<BotCommand> list = new ArrayList<>();
+        list.add(new BotCommand("/help", "help"));
+        list.add(new BotCommand("/uuid", "generate uuid"));
+        list.add(new BotCommand("/uuid_status", "uuid generator status"));
+        list.add(new BotCommand("/password", "password generator"));
+        list.add(new BotCommand("/password_status", "password generator status"));
+        try {
+            execute(new SetMyCommands(list, new BotCommandScopeDefault(), null));
+        } catch (Exception ignored) {
+        }
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
         try {
@@ -40,8 +61,8 @@ public final class Helper31Bot extends TelegramLongPollingBot {
             execute(commandFactory.getCommand(update).execute(update));
         } catch (Exception e) {
             try {
-                execute(noticeService.createNotification("Exception:\n<code>" + e.getMessage() + "</code>"));
-                execute(noticeService.createNotification("<code>" + Arrays.toString(e.getStackTrace()) + "</code>"));
+                execute(noticeService.createNotification(
+                        "<code>" + e.getMessage() + "\n" + Arrays.toString(e.getStackTrace()) + "</code>"));
             } catch (Exception s) {
                 log.error("Exception:", s);
             }
