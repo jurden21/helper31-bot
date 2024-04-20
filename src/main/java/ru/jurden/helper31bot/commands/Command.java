863a16bbfea0b3c2ff72620be89b8af92d830903
jurden <dev@jurden.ru>
2024-04-20 16:25:30 +0300

    Added command to status text

--- src/main/java/ru/jurden/helper31bot/commands/Command.java
+++ src/main/java/ru/jurden/helper31bot/commands/Command.java
@@ -13,14 +13,15 @@ public abstract class Command {
 
     protected CommandState state = CommandState.READY;
 
-    abstract public SendMessage execute(Update update);
+    public abstract SendMessage execute(Update update);
 
     protected String getUuidStatus(UuidSettings settings) {
         return
                 "<b>UUID Generator Settings</b>\n" +
-                        String.format("<code>Hyphens:   %s</code>  (/uuid_hyphens)\n", BooleanUtils.toString(settings.isUseHyphens(), "ON", "OFF")) +
-                        String.format("<code>UpperCase: %s</code>  (/uuid_uppercase)\n", BooleanUtils.toString(settings.isUseUpperCase(), "ON", "OFF")) +
-                        String.format("<code>Braces:    %s</code>  (/uuid_braces)\n", BooleanUtils.toString(settings.isUseBraces(), "ON", "OFF"));
+                        String.format("<code>Hyphens:   %s</code>  (/uuid_hyphens)%n", BooleanUtils.toString(settings.isUseHyphens(), "ON", "OFF")) +
+                        String.format("<code>UpperCase: %s</code>  (/uuid_uppercase)%n", BooleanUtils.toString(settings.isUseUpperCase(), "ON", "OFF")) +
+                        String.format("<code>Braces:    %s</code>  (/uuid_braces)%n", BooleanUtils.toString(settings.isUseBraces(), "ON", "OFF")) +
+                        String.format("%nUse: /uuid");
     }
 
     public void resetState() {
@@ -34,34 +35,36 @@ public abstract class Command {
     protected String getPasswordStatus(PasswordSettings settings) {
         return
                 "<b>Password Generator Settings</b>\n" +
-                        String.format("<code>Length:    %d</code>  (/password_length)\n", settings.getLength()) +
-                        String.format("<code>UpperCase: %s</code>  (/password_uppercase)\n", BooleanUtils.toString(settings.isUseUpperCase(), "ON", "OFF")) +
-                        String.format("<code>LowerCase: %s</code>  (/password_lowercase)\n", BooleanUtils.toString(settings.isUseLowerCase(), "ON", "OFF")) +
-                        String.format("<code>Digits:    %s</code>  (/password_digits)\n", BooleanUtils.toString(settings.isUseDigits(), "ON", "OFF")) +
-                        String.format("<code>Special:   %s</code>  (/password_special)\n", BooleanUtils.toString(settings.isUseSpecial(), "ON", "OFF"));
+                        String.format("<code>Length:    %d</code>  (/password_length)%n", settings.getLength()) +
+                        String.format("<code>UpperCase: %s</code>  (/password_uppercase)%n", BooleanUtils.toString(settings.isUseUpperCase(), "ON", "OFF")) +
+                        String.format("<code>LowerCase: %s</code>  (/password_lowercase)%n", BooleanUtils.toString(settings.isUseLowerCase(), "ON", "OFF")) +
+                        String.format("<code>Digits:    %s</code>  (/password_digits)%n", BooleanUtils.toString(settings.isUseDigits(), "ON", "OFF")) +
+                        String.format("<code>Special:   %s</code>  (/password_special)%n", BooleanUtils.toString(settings.isUseSpecial(), "ON", "OFF")) +
+                        String.format("%nUse: /password");
     }
 
     protected String getHelp() {
-        return
-                "Useful tools. Use command /help for additional information.\n\n" +
-
-                        "<b>UUID generator</b>\n" +
-                        "/uuid - generate uuid\n" +
-                        "/uuid_status - uuid generator status\n" +
-                        "/uuid_hyphens - toggle using hyphens\n" +
-                        "/uuid_uppercase - toggle using uppercase\n" +
-                        "/uuid_braces - toggle using braces\n\n" +
-
-                        "<b>Password Generator</b>\n" +
-                        "/password - generate password\n" +
-                        "/password_status - password generator status\n" +
-                        "/password_length - password generator status\n" +
-                        "/password_uppercase - toggle using uppercase\n" +
-                        "/password_lowercase - toggle using lowercase\n" +
-                        "/password_digits - toggle using digits\n" +
-                        "/password_special - toggle using special chars\n\n" +
-
-                        "Bot owner: @unknown31bot";
+        return """
+                 Useful tools. Use command /help for additional information.
+                 \s
+                 <b>UUID generator</b>
+                 /uuid - generate uuid
+                 /uuid_status - uuid generator status
+                 /uuid_hyphens - toggle using hyphens
+                 /uuid_uppercase - toggle using uppercase
+                 /uuid_braces - toggle using braces
+                 \s
+                 <b>Password Generator</b>
+                 /password - generate password
+                 /password_status - password generator status
+                 /password_length - password generator status
+                 /password_uppercase - toggle using uppercase
+                 /password_lowercase - toggle using lowercase
+                 /password_digits - toggle using digits
+                 /password_special - toggle using special chars
+                 \s
+                 Bot owner: @unknown31bot
+                """;
     }
 
 }
